5896281d90b424046ec9c0ab5e29dfbc07a6470b
予纾 <100036558+Yuisyuu@users.noreply.github.com>
2024-05-07 10:01:41 +0800

    chore: Rename files name

--- src/plugin/PureLevel.cpp
+++ src/plugin/PureLevel.cpp
@@ -1,34 +1,34 @@
-#include "plugin/MyPlugin.h"
+#include "plugin/PureLevel.h"
 
 #include <memory>
 
 #include "ll/api/plugin/NativePlugin.h"
 #include "ll/api/plugin/RegisterHelper.h"
 
-namespace my_plugin {
+namespace pure_level {
 
-static std::unique_ptr<MyPlugin> instance;
+static std::unique_ptr<PureLevel> instance;
 
-MyPlugin& MyPlugin::getInstance() { return *instance; }
+PureLevel& PureLevel::getInstance() { return *instance; }
 
-bool MyPlugin::load() {
+bool PureLevel::load() {
     getSelf().getLogger().info("Loading...");
     // Code for loading the plugin goes here.
     return true;
 }
 
-bool MyPlugin::enable() {
+bool PureLevel::enable() {
     getSelf().getLogger().info("Enabling...");
     // Code for enabling the plugin goes here.
     return true;
 }
 
-bool MyPlugin::disable() {
+bool PureLevel::disable() {
     getSelf().getLogger().info("Disabling...");
     // Code for disabling the plugin goes here.
     return true;
 }
 
-} // namespace my_plugin
+} // namespace pure_level
 
-LL_REGISTER_PLUGIN(my_plugin::MyPlugin, my_plugin::instance);
+LL_REGISTER_PLUGIN(pure_level::PureLevel, pure_level::instance);
--- src/plugin/PureLevel.h
+++ src/plugin/PureLevel.h
@@ -2,14 +2,14 @@
 
 #include "ll/api/plugin/NativePlugin.h"
 
-namespace my_plugin {
+namespace pure_level {
 
-class MyPlugin {
+class PureLevel {
 
 public:
-    static MyPlugin& getInstance();
+    static PureLevel& getInstance();
 
-    MyPlugin(ll::plugin::NativePlugin& self) : mSelf(self) {}
+    PureLevel(ll::plugin::NativePlugin& self) : mSelf(self) {}
 
     [[nodiscard]] ll::plugin::NativePlugin& getSelf() const { return mSelf; }
 
@@ -30,4 +30,4 @@ private:
     ll::plugin::NativePlugin& mSelf;
 };
 
-} // namespace my_plugin
+} // namespace pure_level
